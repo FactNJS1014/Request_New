a4dbab873714ea979c7dc626a324124827de28d9
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-03-03 16:15:58 +0700

    update use vue-router for date 03-03-2025

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,13 +1,16 @@
 <template>
-    <aside class="fixed top-0 left-0 h-screen bg-blue-500 max-w-64">
-      <div class="p-4 text-white">
-        <div class="flex items-center gap-3">
-          <span class="icon-[tabler--tag-filled] text-2xl"></span>
-          <h3 class="text-2xl font-bold">QUOTATION</h3>
-        </div>
+  <aside id="logo-sidebar"
+    class="fixed top-0 left-0 h-screen bg-blue-500 overlay sm:shadow-none overlay-open:translate-x-0 drawer drawer-start max-w-64 sm:z-0 sm:flex sm:translate-x-0"
+    role="dialog" tabindex="-1">
+    <div class="text-white drawer-header">
+      <div class="flex items-center gap-3 p-0">
+        <span class="icon-[tabler--tag-filled] text-2xl"></span>
+        <h3 class="text-2xl font-bold text-white drawer-title">QUOTATION</h3>
       </div>
-      <div class="flex flex-col justify-between h-full px-2">
-        <ul class="p-0 space-y-4 text-xl font-semibold menu">
+    </div>
+    <div class="flex flex-col justify-between h-full px-2 drawer-body">
+      <ul class="menu p-0 text-xl space-y-[230px] font-semibold">
+        <div class="p-4 space-y-4">
           <li>
             <router-link to="/" class="flex items-center gap-2">
               <span class="icon-[tabler--book-filled] text-2xl"></span>
@@ -38,25 +41,28 @@
               Manual
             </router-link>
           </li>
+        </div>
+        <div class="p-4">
           <li>
             <router-link to="/main-menu" class="flex items-center gap-2">
               <span class="icon-[tabler--logout-2] text-2xl"></span>
               Main Menu
             </router-link>
           </li>
-        </ul>
-      </div>
-    </aside>
-  </template>
-  
-  <script>
-  export default {
-    name: "Sidebar"
-  };
-  </script>
-  
-  <style scoped>
-  .menu li {
-    padding: 10px;
-  }
-  </style>
+        </div>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+};
+</script>
+
+<style scoped>
+.menu li {
+  padding: 10px;
+}
+</style>
